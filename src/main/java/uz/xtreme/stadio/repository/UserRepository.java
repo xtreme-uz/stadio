93c5a305c4336fd35f9f69689eb55c4fd2e9eb51
rustambekov <rustambekov.mail.box@gmail.com>
2022-02-11 21:13:25 +0500

    fix: initial db data

--- src/main/java/uz/xtreme/stadio/repository/UserRepository.java
+++ src/main/java/uz/xtreme/stadio/repository/UserRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uz.xtreme.stadio.domain.User;
 
+import java.util.Optional;
+
 @Repository
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
